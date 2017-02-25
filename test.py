4d6fb5551318c74676c542b370bff7bcb40ff750
Marien Raat <marienraat@riseup.net>
2017-02-25 16:46:09 +0100

    Change formatting to comply with pep8

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import json
 
 competion_id = 433
 base_link = 'http://api.football-data.org/v1/'
-f = req.urlopen(base_link + 'competitions/' + str(competion_id) + '/leagueTable')
+f = req.urlopen(base_link + 'competitions/' +
+                str(competion_id) + '/leagueTable')
 json_str = f.read().decode('utf-8')
 standings = json.loads(json_str)
 
 
 for standing in standings['standing']:
-    print(standing["teamName"]
-          + ((40 - len(standing['teamName'])) * ' '
-             + str(standing['points'])))
-
+    print(standing['teamName'] +
+          ((40 - len(standing['teamName'])) * ' ' +
+           str(standing['points'])))
