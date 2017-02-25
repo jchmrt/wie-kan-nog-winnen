52f42eecf99c6fb4d2c077820794d7450556b995
Marien Raat <marienraat@riseup.net>
2017-02-25 17:28:24 +0100

    Replace push with append

--- schedule.py
+++ schedule.py
@@ -3,15 +3,15 @@ class Schedule:
         self.games = []
 
     def add_game(self, team_1, team_2):
-        self.games.push((team_1, team_2))
+        self.games.append((team_1, team_2))
 
     def find_opponents(self, team):
         opponents = []
 
         for (team_1, team_2) in self.games:
             if team_1 == team:
-                opponents.push(team_2)
+                opponents.append(team_2)
             else if team_2 == team:
-                opponents.push(team_1)
+                opponents.append(team_1)
 
         return opponents
