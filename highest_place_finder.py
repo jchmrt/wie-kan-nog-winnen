f9b6bd84060852db71cde408eae87dcd48f3fa66
Marien Raat <marienraat@riseup.net>
2017-02-26 01:36:16 +0100

    Add brute force for remaining games

--- highest_place_finder.py
+++ highest_place_finder.py
@@ -6,7 +6,7 @@ class HighestPlaceFinder:
         my_team = simulation_team.copy()
         self.current_state = self.league_state.copy()
 
-        my_max_points = my_team.get_max_points(self.current_state.schedule)
+        self.my_max_points = my_team.get_max_points(self.current_state.schedule)
 
         self.win_games(my_team)
 
@@ -18,20 +18,12 @@ class HighestPlaceFinder:
                     len(self.current_state.schedule.
                         get_games_with(team.team_name))
                 if (team.get_max_points(self.current_state.schedule) <=
-                    my_max_points)\
+                    self.my_max_points)\
                    and (ammount_of_games_with > 0):
                     self.win_games(team)
                     found = True
 
-        # TODO: Add bruteforce part
-
-        place = 1
-        for team in self.current_state.simulation_teams:
-            if team.get_max_points(self.current_state.schedule) >\
-               my_max_points:
-                place += 1
-
-        return place
+        return self.find_best_place_from(self.current_state)
 
     def win_games(self, team):
         games_with = self.current_state.schedule.get_games_with(team.team_name)
@@ -39,3 +31,34 @@ class HighestPlaceFinder:
         for game in games_with:
             team.win_game()
             self.current_state.schedule.remove_game(game)
+
+    def find_best_place_from(self, state):
+        if not state.schedule.games:
+            return self.get_place(state)
+
+        game = state.schedule.games[0]
+        state.schedule.games.remove(game)
+
+        state_win_home = state.copy()
+        state_win_home.find_simulation_team(game[0]).win_game()
+        best_place_win_home = self.find_best_place_from(state_win_home)
+
+        state_tie = state.copy()
+        state_tie.find_simulation_team(game[0]).tie_game()
+        state_tie.find_simulation_team(game[1]).tie_game()
+        best_place_tie = self.find_best_place_from(state_tie)
+
+        state_win_away = state.copy()
+        state_win_away.find_simulation_team(game[1]).win_game()
+        best_place_win_away = self.find_best_place_from(state_win_away)
+
+        return max([best_place_win_home,
+                    best_place_tie,
+                    best_place_win_away])
+
+    def get_place(self, state):
+        place = 1
+        for team in state.simulation_teams:
+            if team.points > self.my_max_points:
+                place += 1
+        return place
--- league_state.py
+++ league_state.py
@@ -9,3 +9,8 @@ class LeagueState:
             simulation_teams_copy.append(team.copy())
         l = LeagueState(self.schedule.copy(), simulation_teams_copy)
         return l
+
+    def find_simulation_team(self, team_name):
+        for team in self.simulation_teams:
+            if team.team_name == team_name:
+                return team
