a11af41b549129aca045fc2a380b62e6b739872a
Marien Raat <marienraat@riseup.net>
2017-02-27 19:27:12 +0100

    Add license

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+# Wie kan nog winnen is a collection of utilities to calculate and
+# display what places teams of the Eredivisie can reach if everything
+# goes perfectly.
+# Copyright (C) 2017  Jochem Raat
+# Copyright (C) 2017  Marien Raat
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import urllib.request as req
 import json
 import simulation_team
